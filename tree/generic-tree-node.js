86dbdfe8e2e4f270bb1f583b3fb7ae71d71352aa
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2019-10-01 20:52:15 +0300

    add tests for generic tree

--- tree/generic-tree-node.js
+++ tree/generic-tree-node.js
@@ -2,14 +2,16 @@ const data = Symbol('data');
 const childrens = Symbol('childrens');
 const parent = Symbol('parent');
 
+// TODO: add validation for cases when node already assign to parrent, and we try to setParent again
 // TODO: add type-checking(most likely flow)
 class GenericTreeNode {
   constructor(nodeData) {
     this[data] = nodeData;
-    this[childrens] = null;
+    this[childrens] = [];
     this[parent] = null;
   }
 
+  // TODO: think about advantages which "Symbol-private" props gave as if we use get
   get data() {
     return this[data];
   }
@@ -18,17 +20,24 @@ class GenericTreeNode {
     return this[parent];
   }
 
-  set parent(node) {
-    this[parent] = node;
-  }
-
   get childrens() {
     return this[childrens];
   }
 
+  setParent(node) {
+    if (this[parent]) {
+      this[parent].removeChildrenNode(this);
+    }
+    node.addChildrenNode(this);
+  }
+
   addChildrenNode(node) {
-    node.setParentNode = this;
+    node[parent] = this;
     this[childrens].push(node);
+  }
+
+  removeChildrenNode(node) {
+    this[childrens] = this[childrens].filter(childNode => childNode !== node);
   }
 }
 
--- tree/generic-tree-node.test.js
+++ tree/generic-tree-node.test.js
@@ -11,14 +11,41 @@ describe('GenericTreeNode', () => {
   it('get node data', () => {
     expect(rootNode.data).toEqual('root');
   })
-  it('get node data', () => {
-    expect(rootNode.data).toEqual('root');
+
+  it('add children node', () => {
+    rootNode.addChildrenNode(new GenericTreeNode('child'));
+
+    expect(rootNode.childrens.length).toEqual(1);
   })
-  it('get node data', () => {
-    expect(rootNode.data).toEqual('root');
+
+  it('remove children node', () => {
+    const childNode = new GenericTreeNode('child');
+
+    rootNode.addChildrenNode(childNode);
+    expect(rootNode.childrens.length).toEqual(1);
+
+    rootNode.removeChildrenNode(childNode);
+    expect(rootNode.childrens.length).toEqual(0);
   })
-  it('get node data', () => {
-    expect(rootNode.data).toEqual('root');
+
+  it('get parent node', () => {
+    const childNode = new GenericTreeNode('child');
+    rootNode.addChildrenNode(childNode);
+
+    expect(childNode.parent == rootNode);
+  })
+
+  it('setParent', () => {
+    const childNode = new GenericTreeNode('child');
+    const anotherNode = new GenericTreeNode('another');
+
+    rootNode.addChildrenNode(childNode);
+    expect(rootNode.childrens[0].parent == rootNode);
+
+    childNode.setParent(anotherNode);
+    expect(rootNode.childrens.length).toEqual(0);
+    expect(anotherNode.childrens[0].parent == rootNode);
+    expect(childNode.parent == anotherNode);
   })
 
 });
