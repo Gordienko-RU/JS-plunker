2ed4bfbf1974bf255615595c0cd09160f6798c65
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2019-10-02 16:09:20 +0300

    start to implementing DFS algorithm

---
+++ tree/build-tree-from-object.js
@@ -0,0 +1,10 @@
+const GenericTreeNode = require('./generic-tree-node');
+
+// TODO: implement recursive Depth First Search to perform object transformation to Tree 
+// const buildTreeFromObject = (obj) => {
+//   const rootKey = Object.keys(obj)[0];
+//   const rootNode = GenericTreeNode(rootKey);
+//   const bodyTreeKeys = Object.keys(obj[rootKey]);
+
+//   bodyTreeKeys.forEach((key) => );
+// }
---
+++ tree/build-tree-from-object.test.js
@@ -0,0 +1,16 @@
+const nestedObject = {
+  root: {
+    first: {
+      child1: 'child1',
+      child2: 'child2',
+      child3: 'child3',
+    },
+    second: {
+      child1: 'child1',
+    },
+    third: {
+      child2: 'child2',
+      child3: 'child3',
+    },
+  }
+}
