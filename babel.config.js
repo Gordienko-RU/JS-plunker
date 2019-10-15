a9001e000e13f4593c570c5c16605f05fefd002f
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2019-10-15 16:42:25 +0300

    add shallow babel config, add TODOs

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+const presets = [['@babel/env']];
+const plugins = ['@babel/plugin-proposal-decorators'];
+
+// TODO: set-up correct babel transformation for trying new features
+module.exports = { presets, plugins };
