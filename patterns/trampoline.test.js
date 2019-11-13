e9abaaaae54e5208da637f6cb4d4d8b6c114f9d7
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2019-11-13 18:10:34 +0300

    implement trampoline

---
+++ patterns/trampoline.test.js
@@ -0,0 +1,21 @@
+const trampoline = fn => (...args) => {
+  let value = fn(...args);
+
+  while(typeof value === 'function') {
+    value = value();
+  }
+
+  return value;
+};
+
+const sumBelow = (n, sum = 0) => () => n === 0
+  ? sum
+  : sumBelow(n - 1, sum + n);
+
+describe('trampoline', () => {
+  it('prevent stack overflow', () => {
+    const wrapped = trampoline(sumBelow);
+
+    console.log(wrapped(100000000));
+  })
+});
