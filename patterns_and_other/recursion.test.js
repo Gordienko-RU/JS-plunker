ecd76292c4b896f5842e7da2bee4f0031b84538e
Roman Gordienko <r.u.gordienko@gmail.com>
2019-11-16 15:27:49 +0300

    one more recursion case

--- patterns_and_other/recursion.test.js
+++ patterns_and_other/recursion.test.js
@@ -6,8 +6,22 @@ function sum(arr) {
   return arr[0] + sum(arr.slice(1));
 }
 
+function getMaxElement(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  const maxElement = getMaxElement(arr.slice(1));
+
+  return arr[0] > maxElement
+    ? arr[0]
+    : maxElement;
+}
+
 describe('recursion', () => {
   it('array handling with recursion', () => {
     expect(sum([1, 2, 3, 4])).toBe(10);
+  })
+  it('array handling with recursion', () => {
+    expect(getMaxElement([7, 0, 3, 6, 0])).toBe(7);
   })
 });
