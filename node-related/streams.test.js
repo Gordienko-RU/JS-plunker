0f82967273dfd629e185617f1b3c405395487494
Roman Gordienko <r.u.gordienko@gmail.com>
2019-12-09 21:42:02 +0300

    basic test for readable stream

---
+++ node-related/streams.test.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+async function handleChunks(cb, stream) {
+  for await(const chunk of stream) {
+    cb(chunk);
+  }
+}
+
+describe('streams', () => {
+  it('handles readable stream', async () => {
+    const readableStream = fs.createReadStream('./node-related/stream.test.txt', { encoding: 'utf-8' });
+    let content;
+
+    await handleChunks(chunk => content += chunk, readableStream);
+    expect(content).toBe('test');
+  })
+})
+
