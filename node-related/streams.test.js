c24400a14a9523147adc87d055ae4f4436829d3a
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2019-12-11 18:22:06 +0300

    create stream from async iterator

--- node-related/streams.test.js
+++ node-related/streams.test.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { Readable } = require('stream');
 
 async function handleChunks(cb, stream) {
   for await(const chunk of stream) {
@@ -19,6 +20,15 @@ describe('streams', () => {
     
     await handleChunks(chunk => content += chunk, readableStream);
     expect(content).toBe('test');
+  })
+  it('create stream from async iterator', async () => {
+    async function* generate() {
+      yield 'test';
+    }
+
+    const readableStream = Readable.from(generate());
+
+    readableStream.on('data', chunk => expect(chunk).toBe('test'));
   })
 })
 
