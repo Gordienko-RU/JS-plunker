9f638bd59be7071fb3f36275e9c74dfbfd9f0abc
Roman Gordienko <r.u.gordienko@gmail.com>
2019-12-22 23:17:24 +0300

    refactoring, add test for pipeline

--- node-related/streams.test.js
+++ node-related/streams.test.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
-const { Readable, finished } = require('stream');
+const { Readable, finished, pipeline } = require('stream');
 const { promisify } = require('util');
 const { once } = require('events');
+const { createGzip } = require('zlib');
 
 async function handleChunks(cb, stream) {
   for await(const chunk of stream) {
     cb(chunk);
   }
 }
+
+const whenFinish = promisify(finished);
 
 describe('streams', () => {
   it('handles readable stream', async () => {
@@ -23,6 +26,7 @@ describe('streams', () => {
     await handleChunks(chunk => content += chunk, readableStream);
     expect(content).toBe('test');
   })
+
   it('create stream from async iterator', async () => {
     async function* generate() {
       yield 'test';
@@ -32,27 +36,22 @@ describe('streams', () => {
 
     readableStream.on('data', chunk => expect(chunk).toBe('test'));
   })
+
   it('creates simple write stream', async () => {
     const writableStream = fs.createWriteStream('./node-related/stream.test2.txt');
 
     writableStream.write('content');
     writableStream.end();
+    await whenFinish(writableStream);
 
-    const readableStream = fs.createReadStream(
-      './node-related/stream.test2.txt',
-      {
-        encoding: 'utf-8',
-        highWaterMark: 2,
-      },
-    );
+    const readableStream = fs.createReadStream('./node-related/stream.test2.txt');
     let content = '';
     
     await handleChunks(chunk => content += chunk, readableStream);
     expect(content).toBe('content');
   })
-  it('create write stream from iterator', async () => {
-    const whenFinish = promisify(finished);
 
+  it('create write stream from iterator', async () => {
     async function writeIterableToFile(iterable, pathToFile) {
       const writableStream = fs.createWriteStream(pathToFile);
 
@@ -68,17 +67,23 @@ describe('streams', () => {
 
     await writeIterableToFile(['data', 'data', 'data'], './node-related/stream.test3.txt');
 
-    const readableStream = fs.createReadStream(
-      './node-related/stream.test3.txt',
-      {
-        encoding: 'utf-8',
-        highWaterMark: 2,
-      },
-    );
+    const readableStream = fs.createReadStream('./node-related/stream.test3.txt');
     let content = '';
     
     await handleChunks(chunk => content += chunk, readableStream);
     expect(content).toBe('datadatadata');
+  })
+
+  it('read -> gzip -> write to destination pipeline', async () => {
+    const pipe = promisify(pipeline);
+
+    const err = await pipe(
+      fs.createReadStream('./node-related/stream.test3.txt'),
+      createGzip(),
+      fs.createWriteStream('./node-related/stream.test3.txt.gz'),
+    );
+    
+    expect(!err);
   })
 })
 
