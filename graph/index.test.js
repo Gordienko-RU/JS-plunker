5fbe667d4f0b46d8a3efb21d4a014807ead3970c
Roman Gordienko <r.u.gordienko@gmail.com>
2020-01-19 19:14:01 +0300

    find shortest way implementation, refactoring

--- graph/index.test.js
+++ graph/index.test.js
@@ -17,6 +17,7 @@ class GraphNode {
    * @param {string} value 
    */
   constructor(value) {
+    // TODO: can we have nodes with same value?
     this.value = value;
     // NOTE: each node contains only info regarding emitted edges
     this.emittedEdges = [];
@@ -32,48 +33,92 @@ class GraphNode {
 }
 
 /**
+ * @param {Object} parents 
+ * @param {string} targetNodeValue
+ * @returns {string []}
+ */
+// TODO: cover with tests
+function getPath(parents, targetNodeValue) {
+  const path = [targetNodeValue];
+  let segment = parents[targetNodeValue];
+
+  while(segment) {
+    path.unshift(segment);
+    segment = parents[segment];
+  }
+
+  return path;
+}
+
+/**
  * @description BFS used to find shortest path
  * @param {GraphNode} startNode
  * @param {string} searchValue
  * @returns {string []}
  */
 function findShortestWay(startNode, searchValue) {
-  if (startNode.value === searchValue) {
-    return [startNode.value];
-  }
+  const { value: startNodeValue, emittedEdges: startNodeEdges } = startNode;
 
+  if (startNodeValue === searchValue) {
+    return [startNodeValue];
+  }
   const queue = [];
   const visitedNodes = [];
   const parentNodes = {};
 
-  queue.push(...startNode.emittedEdges);
-  visitedNodes.push(startNode.value);
+  queue.push(...startNodeEdges);
+  visitedNodes.push(startNodeValue);
   
-  startNode.emittedEdges.forEach((node) => {
-    
+  startNodeEdges.forEach((node) => {
+    parentNodes[node.value] = startNodeValue;
   })
 
   while(queue.length) {
     const currentNode = queue.shift();
-    
-    if (visitedNodes.includes(currentNode.value)) {
+    const { value, emittedEdges } = currentNode;
+
+    if (value === searchValue) {
+      return getPath(parents, value);
+    }
+
+    if (visitedNodes.includes(value)) {
       continue;
     }
 
-
-
-    if () {
-
-    }
+    visitedNodes.push(value);
+    queue.push(...emittedEdges);
   }
+
+  throw new Error(`node ${searchValue} doesn't exist`);
 }
 
+let startNode = null;
+
 describe('Graph', () => {
+  beforeEach(() => {
+    startNode = new GraphNode('A');
+    const node1 = new GraphNode('B');
+
+    const node2 = new GraphNode('C');
+    const node3 = new GraphNode('D');
+    const node4 = new GraphNode('F');
+    const node5 = new GraphNode('G');
+
+    startNode.setEdgeTo(node1, 5);
+    startNode.setEdgeTo(node2, 2);
+    node2.setEdgeTo(node1, 2);
+    node2.setEdgeTo(node3, 7);
+    node3.setEdgeTo(node4, 5);
+    node4.setEdgeTo(node5, 7);
+    node1.setEdgeTo(node5, 34);
+  })
+
   it('creates graph node', () => {
     const node = new GraphNode('A');
 
     expect(node.value).toBe('A');
   })
+
   it('creates directed weighted edge', () => {
     const node1 = new GraphNode('A');
     const node2 = new GraphNode('B');
@@ -81,5 +126,9 @@ describe('Graph', () => {
     node1.setEdgeTo(node2, 34);
     expect(node1.emittedEdges[0].node).toBe(node2);
     expect(node1.emittedEdges[0].weight).toBe(34);
+  })
+
+  it('finds the shortest path to the node', () => {
+    console.log('shortest path', findShortestWay(startNode, 'G'));
   })
 })
