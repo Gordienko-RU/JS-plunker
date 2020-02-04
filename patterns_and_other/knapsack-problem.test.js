49d7b6e377a9b2608f41cbc0b81f19e2f5dfb51d
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2020-02-04 18:06:47 +0300

    naive knapsack problem implementation

--- patterns_and_other/knapsack-problem.test.js
+++ patterns_and_other/knapsack-problem.test.js
@@ -18,11 +18,43 @@ const testItems = [
 
 /**
  * @description solving classical knapsack problem with integer weight
- * @param {*} containerSize 
- * @param {*} itemsToPick 
+ * @param {number} containerSize
+ * @param {Item []} itemsToPick
+ * @returns {string []}
  */
 function pickMostValuableSet(containerSize, itemsToPick) {
+  const weightTable = [[]];
 
+  // NOTE: set initial row
+  for (const i = 0; i < containerSize; i += 1) {
+    weightTable[0][i] = 0;
+  }
+
+  itemsToPick.forEach(({ label, value, weight }, rowIndex) => {
+    const columnValues = [];
+
+    // TODO: not clear, rename
+    const relevantWeight = weight - 1;
+
+    for (const columnIndex = 0; columnIndex < containerSize; columnIndex += 1) {
+      const oldMaxCellValue = weightTable[rowIndex][columnIndex];
+
+      if (relevantWeight > columnIndex) {
+        columnValues[columnIndex] = oldMaxCellValue;
+        
+        continue;
+      }
+
+      const maxValueOfFreeCells = weightTable[rowIndex][columnIndex - weight];
+      const potentiallyMaxValue = maxValueOfFreeCells + value;
+
+      columnValues[columnIndex] = potentiallyMaxValue > oldMaxCellValue
+        ? potentiallyMaxValue
+        : oldMaxCellValue;
+    }
+  });
+
+  console.log('table', JSON.stringify(weightTable, null, 2));
 }
 
 describe('pick the most valuable set of items', () => {
