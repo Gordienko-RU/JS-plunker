cafe8ce1c9cc35776a87c91dfde3a7bd14b6908d
Roman Gordienko <roman_gordienko@external.mckinsey.com>
2020-03-03 20:42:00 +0300

    start implementing algorithm

--- patterns_and_other/find-longest-substring-for-collection.test.js
+++ patterns_and_other/find-longest-substring-for-collection.test.js
@@ -5,8 +5,33 @@ const subStrings = [
   'ride',
 ];
 
-function findCommonSubString(array) {
-  
+function findCommonSubString(strings) {
+  const pivot = strings.sort((a, b) => b.length - a.length).pop();
+  let longestSubstring = '';
+  const lastVisitedLatterByIndex = [];
+  let lastMatchedLetterIndex = null;
+
+  Array.from(pivot).forEach((searchLetter) => {
+    let isLetterCommon = true;
+
+    for (let i = 0; i < strings.length; i += 1) {
+      const matchIndex = strings[i].indexOf(searchLetter);
+
+      if (matchIndex === -1) {
+        isLetterCommon = false;
+        break;
+      }
+
+      lastVisitedLatterByIndex[i] = matchIndex;
+    }
+    const isSubsequenceContinued = lastMatchedLetterIndex === (i - 1);
+    const isNewSubsequenceLonger = longest
+
+    if (isLetterCommon) {
+
+    }
+  });
+
 }
 
 describe('findCommonSubstring', () => {
